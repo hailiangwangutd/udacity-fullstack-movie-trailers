67cb79decc9f0ce77d557e6dc722383a47100216
neal_wang <hailiangwangutd@gmail.com>
2015-10-22 14:05:20 -0500

    Movie Trailers

--- media.py
+++ media.py
@@ -2,16 +2,18 @@ import webbrowser
 import fresh_tomatoes
 import csv
 
+
 class Movie():
     """ This class provides a way to store movie related information"""
-    
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+
+    def __init__(self, title, movie_actor, poster_image_url, trailer_youtube_url):
         self.title = title
-        self.storyline = storyline
+        self.movie_actor = movie_actor
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+
 
 def get_movies(filename):
     """ pulls movie data from csv and returns a list a Movie objects """
@@ -19,8 +21,10 @@ def get_movies(filename):
     with open('data.csv', 'r') as csvfile:
         spamreader = csv.reader(csvfile)
         for row in spamreader:
+
             movies.append(Movie(row[0], row[1], row[2], row[3]))
     return movies
+
 
 def main():
     """ main subroutine """
